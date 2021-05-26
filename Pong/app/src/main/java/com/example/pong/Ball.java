1fee4730ca91904bcd67f7617a9874bbfb69f100
Azwark <30425457+Azwark@users.noreply.github.com>
2021-05-25 23:34:54 -0400

    pong game finished, need comments

--- Pong/app/src/main/java/com/example/pong/Ball.java
+++ Pong/app/src/main/java/com/example/pong/Ball.java
@@ -1,4 +1,86 @@
 package com.example.pong;
 
+import android.graphics.RectF;
+
 public class Ball {
+
+    // member variables
+    private RectF mRect;
+    private float mXVelocity;
+    private float mYVelocity;
+    private float mBallWidth;
+    private float mBallHeight;
+
+    // Constructor
+    public Ball(int screenX){
+        // make ball 1% of screen
+        mBallWidth = screenX / 100;
+        mBallHeight = screenX / 100;
+
+        mRect = new RectF();
+    }
+
+    // getter for mRect
+    RectF getRect(){
+        return mRect;
+    }
+
+    // update position of the ball
+    void update(long fps){
+        mRect.left = mRect.left + (mXVelocity / fps);
+        mRect.top = mRect.top + (mYVelocity / fps);
+        mRect.right = mRect.left + mBallWidth;
+        mRect.bottom = mRect.top + mBallHeight;
+    }
+
+    // Reverses Y velocity
+    void reverseYVelocity(){
+        mYVelocity = -mYVelocity;
+    }
+
+    // Reverses X velocity
+    void reverseXVelocity(){
+        mXVelocity = -mXVelocity;
+    }
+
+    // Resets ball position
+    void reset(int x, int y){
+        // set pos and size
+        mRect.left = x / 2;
+        mRect.top = 0;
+        mRect.right = x / 2 + mBallWidth;
+        mRect.bottom = mBallHeight;
+
+        // set initial velocities
+        mYVelocity = (y / 3); // remove -
+        mXVelocity = (x / 2);
+
+    }
+
+    // Increase ball speed by 10%
+    void increaseVelocity(){
+        mXVelocity = mXVelocity * 1.1f;
+        mYVelocity = mYVelocity * 1.1f;
+
+    }
+
+    void batBounce(RectF batPosition){
+
+        // detect centre of bat
+        float batCenter = batPosition.left + (batPosition.width() / 2);
+
+        // detect the centre of the ball
+        float ballCenter = mRect.left + (mBallWidth / 2);
+
+        // Where did ball hit
+        float relativeIntersect = (batCenter - ballCenter);
+
+        if(relativeIntersect < 0){ //right
+            mXVelocity = Math.abs(mXVelocity);
+        }else{ // left
+            mXVelocity = -Math.abs(mXVelocity);
+        }
+
+        reverseYVelocity();
+    }
 }
--- Pong/app/src/main/java/com/example/pong/Bat.java
+++ Pong/app/src/main/java/com/example/pong/Bat.java
@@ -1,4 +1,73 @@
 package com.example.pong;
 
+import android.graphics.RectF;
+
 public class Bat {
+    private RectF mRect;
+    private float mLength;
+    private float mXCoord;
+    private float mBatSpeed;
+    private int mScreenX;
+
+    final int STOPPED = 0;
+    final int LEFT = 1;
+    final int RIGHT = 2;
+
+    // Starting with STOPPED condition
+    private int mBatMoving = STOPPED;
+
+    // constructor
+    public Bat(int sx, int sy){
+        mScreenX = sx;
+
+        mLength = mScreenX / 8;
+
+        float height = sy / 40;
+
+        mXCoord = mScreenX / 2;
+
+        float mYCoord = sy - height;
+
+        mRect = new RectF(mXCoord, mYCoord,
+                mXCoord + mLength,
+                mYCoord + height);
+
+        mBatSpeed = mScreenX;
+    }
+
+    // getter for mRect
+    RectF getRect(){
+        return mRect;
+    }
+
+    // used to move or stop bat
+    void setMovementState(int state){
+        mBatMoving = state;
+    }
+
+    // update bat, called every frame
+    void update(long fps){
+        // Move the bat based on the mBatMoving variable
+        // and the speed of the previous frame
+        if(mBatMoving == LEFT){
+            mXCoord = mXCoord - mBatSpeed / fps;
+        }
+
+        if(mBatMoving == RIGHT){
+            mXCoord = mXCoord + mBatSpeed / fps;
+        }
+
+        // Stop the bat going off the screen
+        if(mXCoord < 0){
+            mXCoord = 0;
+        }
+        else if(mXCoord + mLength > mScreenX){
+            mXCoord = mScreenX - mLength;
+        }
+
+        // Update mRect based on the results from
+        // the previous code in update
+        mRect.left = mXCoord;
+        mRect.right = mXCoord + mLength;
+    }
 }
