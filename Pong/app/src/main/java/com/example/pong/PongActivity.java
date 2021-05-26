35f57bc3b2b651f4be761dc759e234dac7ffcad1
Azwark <30425457+Azwark@users.noreply.github.com>
2021-05-25 22:15:53 -0400

    Starting point

---
+++ Pong/app/src/main/java/com/example/pong/PongActivity.java
@@ -0,0 +1,40 @@
+package com.example.pong;
+
+import android.app.Activity;
+import android.view.Window;
+import android.os.Bundle;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class PongActivity extends Activity {
+    private PongGame mPongGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        // Find screen size
+        display.getSize(size);
+        // Overloaded contructor
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Call from PongGame class
+        mPongGame.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Call from PongGame class
+        mPongGame.pause();
+    }
+}
